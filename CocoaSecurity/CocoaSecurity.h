e89f4ecdaf868c80f679360fa6fb29c4a02fcbe8
William Swinson <willswinson@gmail.com>
2023-11-07 15:01:37 -0500

    - Remove submodules

--- CocoaSecurity/CocoaSecurity.h
+++ CocoaSecurity/CocoaSecurity.h
@@ -83,3 +83,23 @@
 - (NSData *)base64:(NSString *)data;
 - (NSData *)hex:(NSString *)data;
 @end
+
+#pragma mark - Base64.h
+@interface NSData (Base64)
+
++ (NSData *)dataWithBase64EncodedString:(NSString *)string;
+- (NSString *)base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+- (NSString *)base64EncodedString;
+
+@end
+
+
+@interface NSString (Base64)
+
++ (NSString *)stringWithBase64EncodedString:(NSString *)string;
+- (NSString *)base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+- (NSString *)base64EncodedString;
+- (NSString *)base64DecodedString;
+- (NSData *)base64DecodedData;
+
+@end
